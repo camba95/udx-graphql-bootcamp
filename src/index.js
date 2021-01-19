e979adba0e6a5530003da97c32dfc479b5ae39a3
Kevin Alvarez <kevin.alvarez.ortiz.15@gmail.com>
2021-01-19 14:56:30 -0400

    feat: relational data, comments and posts

--- src/index.js
+++ src/index.js
@@ -23,11 +23,64 @@ const users = [
   },
 ];
 
+const posts = [
+  {
+    id: "fake-post-id-00",
+    title: "GraphQL Bootcamp",
+    body: "This post is essential for Tasking team",
+    published: true,
+    author: "fake-user-id-00",
+  },
+  {
+    id: "fake-post-id-01",
+    title: "MongoDB Bootcamp",
+    body: "NoSQL database course",
+    published: true,
+    author: "fake-user-id-01",
+  },
+  {
+    id: "fake-post-id-02",
+    title: "React.js Bootcamp",
+    body: "UI library for SPA",
+    published: true,
+    author: "fake-user-id-02",
+  },
+];
+
+const comments = [
+  {
+    id: "fake-comment-id-00",
+    text: "This works nicely",
+    author: "fake-user-id-01",
+    post: "fake-post-id-00",
+  },
+  {
+    id: "fake-comment-id-01",
+    text: "Glag you liked it",
+    author: "fake-user-id-00",
+    post: "fake-post-id-00",
+  },
+  {
+    id: "fake-comment-id-02",
+    text: "This sucks",
+    author: "fake-user-id-02",
+    post: "fake-post-id-01",
+  },
+  {
+    id: "fake-comment-id-03",
+    text: "Nevermind",
+    author: "fake-user-id-00",
+    post: "fake-post-id-01",
+  },
+];
+
 const typeDefs = `
   type Query {
     me: User!
     post: Post!
-    users: [User!]!
+    posts: [Post!]!
+    users(query: String): [User!]!
+    comments: [Comment!]!
   }
 
   type User {
@@ -35,6 +88,8 @@ const typeDefs = `
     name: String!
     email: String!
     age: Int
+    posts: [Post!]!
+    comments: [Comment!]!
   }
 
   type Post {
@@ -42,6 +97,15 @@ const typeDefs = `
     title: String!
     body: String!
     published: Boolean!
+    author: User!
+    comments: [Comment!]!
+  }
+
+  type Comment {
+    id: ID!
+    text: String!
+    author: User!
+    post: Post!
   }
 `;
 
@@ -57,7 +121,7 @@ const resolvers = {
     },
     post() {
       return {
-        id: "fake-post-id",
+        id: "fake-post-id-00",
         title: "GraphQL Bootcamp",
         body: "This post is essential for Tasking team",
         published: true
@@ -65,8 +129,38 @@ const resolvers = {
     },
     users(parent, args, context, info) {
       return users;
+    },
+    posts(parent, args, context, info) {
+      return posts;
+    },
+    comments() {
+      return comments;
     }
-  }
+  },
+  Post: {
+    author(parent, args, context, info) {
+      return users.find((user) => user.id === parent.author);
+    },
+    comments(parent, args, context, info) {
+      return comments.filter((comment) => comment.post === parent.id);
+    },
+  },
+  Comment: {
+    author(parent, args, context, info) {
+      return users.find((user) => user.id === parent.author);
+    },
+    post(parent, args, context, info) {
+      return posts.find((post) => post.id === parent.post);
+    },
+  },
+  User: {
+    posts(parent, args, context, info) {
+      return posts.filter((post) => post.author === parent.id);
+    },
+    comments(parent, args, context, info) {
+      return comments.filter((comment) => comment.author === parent.id);
+    },
+  },
 };
 
 const server = new GraphQLServer({ typeDefs, resolvers });
