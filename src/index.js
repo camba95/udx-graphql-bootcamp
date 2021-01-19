ea589745639b9d33a821fc1a5f1ddf1bf1177284
Kevin Alvarez <kevin.alvarez.ortiz.15@gmail.com>
2021-01-19 14:08:05 -0400

    feat: post and users queries

--- src/index.js
+++ src/index.js
@@ -2,16 +2,69 @@ import { GraphQLServer } from "graphql-yoga";
 
 // Scalar types: String, Boolean, Int, Float, ID
 
+const users = [
+  {
+    id: "fake-user-id-00",
+    name: "Kevin Alvarez",
+    email: "kevin@mail.com",
+    age: 25
+  },
+  {
+    id: "fake-user-id-01",
+    name: "Cintia Montero",
+    email: "cintia@mail.com",
+    age: 27
+  },
+  {
+    id: "fake-user-id-02",
+    name: "Ricardo Suarez",
+    email: "ricardo@mail.com",
+    age: 31
+  },
+];
+
 const typeDefs = `
   type Query {
-    hello: String!
+    me: User!
+    post: Post!
+    users: [User!]!
+  }
+
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    age: Int
+  }
+
+  type Post {
+    id: ID!
+    title: String!
+    body: String!
+    published: Boolean!
   }
 `;
 
 const resolvers = {
   Query: {
-    hello() {
-      return "Hello World"
+    me() {
+      return {
+        id: "fake-user-id-00",
+        name: "Kevin Alvarez",
+        email: "kevin@mail.com",
+        age: 25
+      };
+    },
+    post() {
+      return {
+        id: "fake-post-id",
+        title: "GraphQL Bootcamp",
+        body: "This post is essential for Tasking team",
+        published: true
+      };
+    },
+    users(parent, args, context, info) {
+      return users;
     }
   }
 };
