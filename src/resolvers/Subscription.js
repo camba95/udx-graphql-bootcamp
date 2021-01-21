efb08fc948892cd56d97368ada11d5331cc2b79b
Kevin Alvarez <kevin.alvarez.ortiz.15@gmail.com>
2021-01-20 23:17:22 -0400

    feat: use prisma for subscription

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,17 +1,27 @@
 
 const Subscription = {
   comment: {
-    subscribe(parent, { postId }, { pubSub, db }, info) {
-      const post = db.posts.find((post) => post.id === postId && post.published);
-      if (!post) {
-        throw new Error("Post not found.")
-      }
-      return pubSub.asyncIterator(`post:${postId}:comment`);
+    subscribe(parent, { postId }, { prisma }, info) {
+      return prisma.subscription.comment({
+        where: {
+          node: {
+            post: {
+              id: postId
+            }
+          }
+        }
+      }, info);
     }
   },
   post: {
     subscribe(parent, args, { pubSub }, info) {
-      return pubSub.asyncIterator("post");
+      return prisma.subscription.post({
+        where: {
+          node: {
+            published: true
+          }
+        }
+      }, info);
     }
   },
 };
