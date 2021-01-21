fb71483df1746f2057d62ff1df84ab4b98176311
Kevin Alvarez <kevin.alvarez.ortiz.15@gmail.com>
2021-01-21 13:55:25 -0400

    feat: auth for queries

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,25 +1,52 @@
+import getUserId from "../utils/getUserId";
+
 const Query = {
-  me() {
-    return {
-      id: "fake-user-id-00",
-      name: "Kevin Alvarez",
-      email: "kevin@mail.com",
-      age: 25
-    };
+  me: async (parent, args, { prisma, request }, info) => {
+    const userId = getUserId(request);
+    return prisma.query.user({
+      where: { id: userId }
+    });
   },
-  post() {
-    return {
-      id: "fake-post-id-00",
-      title: "GraphQL Bootcamp",
-      body: "This post is essential for Tasking team",
-      published: true
-    };
+  post: async (parent, { id }, { prisma, request }, info) => {
+    const userId = getUserId(request, false);
+
+    const posts = await prisma.query.posts({
+      where: {
+        id,
+        OR: [{
+          published: true
+        }, {
+          author: {
+            id: userId
+          }
+        }]
+      }
+    }, info);
+
+    if (posts.length === 0) {
+      throw new Error("Post not found.");
+    }
+    return posts[0];
   },
   users(parent, args, { prisma }, info) {
     return prisma.query.users(null, info);
   },
   posts(parent, args, { prisma }, info) {
-    return prisma.query.posts(null, info);
+    return prisma.query.posts({
+      where: {
+        published: true
+      }
+    }, info);
+  },
+  myPosts(parent, args, { prisma, request }, info) {
+    const userId = getUserId(request);
+    return prisma.query.posts({
+      where: {
+        author: {
+          id: userId
+        }
+      }
+    }, info);
   },
   comments(parent, args, { prisma }, info) {
     return prisma.query.comments(null, info);
