6251b7e8fcc2ddfc407a59a484cee030eab055c9
Kevin Alvarez <kevin.alvarez.ortiz.15@gmail.com>
2021-01-23 17:26:51 -0400

    feat: fetch data from client

--- client/src/index.js
+++ client/src/index.js
@@ -1,1 +1,65 @@
-console.info("hello world");
+import ApolloBoost, { gql } from "apollo-boost";
+
+const client = new ApolloBoost({
+  uri: "http://localhost:4000"
+});
+
+const getUsers = gql`
+  query {
+    users {
+      id
+      name
+    }
+  }
+`;
+
+const getPosts = gql`
+  query {
+    posts {
+      id
+      title
+      author {
+        name
+      }
+    }
+  }
+`;
+
+client.query({
+  query: getUsers
+})
+  .then(({ data }) => {
+    const { users } = data;
+    let html = "";
+
+    users.forEach((user) => {
+      html += `
+        <div>
+          <h3>${user.name}</h3>
+        </div>
+      `;
+    });
+
+
+    document.getElementById("users").innerHTML = html;
+  });
+
+client.query({
+  query: getPosts
+})
+  .then(({ data }) => {
+    const { posts } = data;
+    let html = "";
+
+    posts.forEach((post) => {
+      html += `
+        <div>
+          <h3>${post.title}</h3>
+          <h4>${post.author.name}</h4>
+          </div>
+      `;
+    });
+
+
+    document.getElementById("posts").innerHTML = html;
+  });
